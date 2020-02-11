dab1bc8c2524a140c75b028822f28b47fecbac10
sneakyluck29 <davidwhitcombtech@gmail.com>
2020-02-11 11:46:50 -0800

    Parsing JSX

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React with Redux</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Todo-App">
+          <form>
+            <input type="tyext" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              <li>
+                <input type="checkbox" /> Create Static UI
+              </li>
+              <li>
+                <input type="checkbox" /> Create Initial State
+              </li>
+              <li>
+                <input type="checkbox" /> Usde State to Render UI
+              </li>
+            </ul>
+          </div>
+        </div>
+
       </header>
     </div>
   );
